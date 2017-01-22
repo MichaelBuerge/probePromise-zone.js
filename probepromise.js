0feaafdf8981af00b2af89fe703bf46f2ec5bb18
Michael Buerge <mib@orino.ch>
2017-01-22 21:39:53 +0100

    Refactor.

--- probepromise.js
+++ probepromise.js
@@ -1,33 +1,46 @@
 
 
-function probePromise(tag) {
-  var self = arguments.callee['static'];
-  if (!self) {
-    self = arguments.callee['static'] = {
-      num: -1,
-      probes: [],
+window['probePromise'] = (function() {
+  var probeId = -1;
+  var probes = [];
+
+  function gatherProbe(tag) {
+    probeId++;
+    var probe = {
+      id: probeId,
+      tag: tag || 'probe' + probeId,
+      zoneJsPresent: false,
+      Promise: null,
+      promiseName: null,
+      NativePromise: null,
+      ZoneAwarePromise: null,
+      promise: null,
+      fetchResult: null,
+    };
+    probes.push(probe);
+
+    probe.Promise = window['Promise'];
+    probe.promiseName = probe.Promise.name;
+
+    probe.zoneJsPresent = !!window['__zone_symbol__Promise'];
+    if (probe.zoneJsPresent) {
+      probe.NativePromise = window['__zone_symbol__Promise'];
+      probe.ZoneAwarePromise = Promise;
+    } else {
+       probe.NativePromise = Promise;
     }
+
+    probe.promise = new Promise(() => null);
+    probe.fetchResult = window['fetch']('');
+
+    return probe;
   }
-  self.num++;
 
-  var probe = {
-    id: self.num,
-    tag: tag || 'probe' + self.num,
-    zoneJsPresent: false,
-    Promise: null,
-    promiseName: null,
-    NativePromise: null,
-    ZoneAwarePromise: null,
-    promise: null,
-    fetchResult: null,
-  };
-  self.probes.push(probe);
+  function logProbe(probe) {
+    var isNativePromise = obj => obj instanceof probe.NativePromise;
+    var isZoneAwarePromise = obj => !!probe.ZoneAwarePromise &&
+                                    obj instanceof probe.ZoneAwarePromise;
 
-  var isNativePromise = obj => obj instanceof probe.NativePromise;
-  var isZoneAwarePromise = obj => !!probe.ZoneAwarePromise &&
-                                  obj instanceof probe.ZoneAwarePromise;
-
-  var logProbe = function() {
     var out = [];
     var log = msg => out.push('| ' + msg);
     out.push('----------- Promise probe: {tag} -----------'.replace('{tag}', probe.tag));
@@ -44,8 +57,8 @@ function probePromise(tag) {
     log('promise is of NativePromise:    ' + isNativePromise(probe.promise));
     log('promise is of ZoneAwarePromise: ' + isZoneAwarePromise(probe.promise));
     
-    log('fetchResult is of NativePromise):    ' + isNativePromise(probe.fetchResult));
-    log('fetchResult is of ZoneAwarePromise): ' + isZoneAwarePromise(probe.fetchResult));
+    log('fetchResult is of NativePromise:    ' + isNativePromise(probe.fetchResult));
+    log('fetchResult is of ZoneAwarePromise: ' + isZoneAwarePromise(probe.fetchResult));
     if (!isNativePromise(probe.fetchResult) && !isZoneAwarePromise(probe.fetchResult)) {
       log('!!! fetchResult is neither instance of NativePromise nor ZoneAwarePromise');
       var Ctor = probe.fetchResult.constructor;
@@ -53,22 +66,11 @@ function probePromise(tag) {
       log('!!! fetchResult.constructor.toString(): ' + Ctor.toString());
     }
 
-    console.log(out.join('\n'));
+    console.log(out.join('\n'));    
   }
 
-  probe.Promise = window['Promise'];
-  probe.promiseName = probe.Promise.name;
-
-  probe.zoneJsPresent = !!window['__zone_symbol__Promise'];
-  if (probe.zoneJsPresent) {
-    probe.NativePromise = window['__zone_symbol__Promise'];
-    probe.ZoneAwarePromise = Promise;
-  } else {
-     probe.NativePromise = Promise;
+  return function(tag) {
+    var probe = gatherProbe(tag);
+    logProbe(probe);
   }
-
-  probe.promise = new Promise(() => null);
-  probe.fetchResult = window['fetch']('');
-
-  logProbe();
-};
+})();
